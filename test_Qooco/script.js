1ef64213069252c4a2bcbc4ded17e66881bb4684
Molchanov Olexandr <35301933+Mol4anovOle@users.noreply.github.com>
2019-03-05 12:46:11 +0200

    Update script.js

--- test_Qooco/script.js
+++ test_Qooco/script.js
@@ -13,35 +13,9 @@ for (var i = 0; i < qualSq; i++) {
 let elemscollection = document.getElementsByClassName("circles");
 
 setInterval(function() {
-  for (let i = 0; i < elemscollection.length; i = i + 3) {
+  for (let i = 0; i < elemscollection.length; ++i) {
     elemscollection[i].style.background = "red";
-  }
-  for (let j = 1; j < elemscollection.length; j = j + 3) {
-    elemscollection[j].style.background = "yellow";
-  }
-  for (let k = 2; k < elemscollection.length; k = k + 3) {
-    elemscollection[k].style.background = "blue";
+    elemscollection[i + 1].style.background = "yellow";
+    elemscollection[i + 2].style.background = "blue";
   }
 }, 1000);
-setInterval(function() {
-  for (let i = 0; i < elemscollection.length; i = i + 3) {
-    elemscollection[i].style.background = "yellow";
-  }
-  for (let j = 1; j < elemscollection.length; j = j + 3) {
-    elemscollection[j].style.background = "blue";
-  }
-  for (let k = 2; k < elemscollection.length; k = k + 3) {
-    elemscollection[k].style.background = "red";
-  }
-}, 1100);
-setInterval(function() {
-  for (let i = 0; i < elemscollection.length; i = i + 3) {
-    elemscollection[i].style.background = "blue";
-  }
-  for (let j = 1; j < elemscollection2.length; j = j + 3) {
-    elemscollection[j].style.background = "red";
-  }
-  for (let k = 2; k < elemscollection.length; k = k + 3) {
-    elemscollection[k].style.background = "yellow";
-  }
-}, 1200);
